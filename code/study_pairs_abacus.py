046547d52d93209516ad70d40f8834c6b1023469
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2021-04-06 11:09:19 -0500

    compute center of mass velocity

--- code/study_pairs_abacus.py
+++ code/study_pairs_abacus.py
@@ -21,6 +21,20 @@ def load_box(BoxID=0, fixed_cosmo=True):
     cosmo_data = np.loadtxt("../data/box_cosmo_params.dat", dtype=dtype)
     hubble = cosmo_data['hubble'][BoxID]
     
+    # compute center of mass velocity
+    a = data['vel_A'].copy()
+    b = data['vel_B'].copy()
+    v_cm = data['vel_A'].copy()
+    mass_tot = data['mass_A'] + data['mass_B']
+    for i in range(3):
+        a[:,i] = data['vel_A'][:,i] * data['mass_A']/mass_tot
+        b[:,i] = data['vel_B'][:,i] * data['mass_B']/mass_tot
+        v_cm[:,i] = a[:,i] + b[:,i]
+    data['vel_CM'] = v_cm
+    data['vel_CM_mag'] = np.sqrt(np.sum(v_cm**2, axis=1))
+    
+    
+    
     data['vel_A_mag'] = np.sqrt(np.sum(data['vel_A']**2, axis=1))
     data['vel_B_mag'] = np.sqrt(np.sum(data['vel_B']**2, axis=1))
     data['vel_G_mag'] = np.sqrt(np.sum(data['vel_G']**2, axis=1))
@@ -30,17 +44,21 @@ def load_box(BoxID=0, fixed_cosmo=True):
     data['vel_AB_rad'] = np.sum((data['pos_B'] - data['pos_A'])*(data['vel_B'] - data['vel_A']), axis=1)/data['pos_AB'] #comoving
     data['vel_AB_tan'] = np.sqrt((data['vel_AB']**2 - data['vel_AB_rad']**2))# comoving
     
+    #data['vel_CM'] = data['vel_B']
+    
     #now we compute the radial velocity including the hubble flow
     data['vel_AB_rad'] = data['vel_AB_rad'] + (data['pos_AB'] * hubble)
 
-    # here we compute the compute the dot product between the position vector and the radial velocity
+    # here we compute the dot product between the position vector and the radial velocity
     data['mu'] = np.sum((data['pos_B']-data['pos_A'])*(data['vel_B']), axis=1)/(data['vel_B_mag']*data['pos_AB'])
     data['mu_vv'] = np.sum(data['vel_A']*data['vel_B'], axis=1)/(data['vel_A_mag']*data['vel_B_mag'])
 
-
+    # here we compute the dot product between the position vector and the center of mass velocity
+    data['mu_cm'] = np.sum((data['pos_B']-data['pos_A'])*(data['vel_CM']), axis=1)/(data['vel_CM_mag']*data['pos_AB'])
+    
     datos = {}
     ii = (data['pos_A'][:,0] > 10) & (data['pos_A'][:,0]<710)
-    keys = ['vel_A_mag', 'vel_B_mag', 'pos_AB', 'vel_AB', 'vel_AB_rad', 'vel_AB_tan', 'vmax_A', 'vmax_B', 'mu', 'mu_vv']
+    keys = ['vel_A_mag', 'vel_B_mag', 'pos_AB', 'vel_AB', 'vel_AB_rad', 'vel_AB_tan', 'vmax_A', 'vmax_B', 'mu', 'mu_vv', 'mu_cm', 'vel_CM_mag']
     for kk in keys:
         datos[kk] = data[kk][ii]
     keys = ['vel_G_mag', 'vmax_G']
