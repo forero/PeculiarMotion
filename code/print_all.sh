d7b5b1720793c372e4abc7c7c21581dec175c04e
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2021-11-09 15:07:21 -0500

    script to print all

---
+++ code/print_all.sh
@@ -0,0 +1,22 @@
+python print_velocity_summary.py --sim 0
+python print_velocity_summary.py --sim 1
+python print_velocity_summary.py --sim 2
+python print_velocity_summary.py --sim 3
+python print_velocity_summary.py --sim 4
+python print_velocity_summary.py --sim 5
+python print_velocity_summary.py --sim 6
+python print_velocity_summary.py --sim 7
+python print_velocity_summary.py --sim 8
+python print_velocity_summary.py --sim 9
+python print_velocity_summary.py --sim 10
+python print_velocity_summary.py --sim 11
+python print_velocity_summary.py --sim 12
+python print_velocity_summary.py --sim 13
+python print_velocity_summary.py --sim 14
+python print_velocity_summary.py --sim 15
+python print_velocity_summary.py --sim 16
+python print_velocity_summary.py --sim 17
+python print_velocity_summary.py --sim 18
+python print_velocity_summary.py --sim 19
+
+
